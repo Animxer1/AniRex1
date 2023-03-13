c093a65ca955a7a7a644ebdd1980601c80183d6c
Kiriyako <92046315+Kiriyako@users.noreply.github.com>
2023-03-13 22:38:43 +0530

    made homepage work again. no more satou gif tho-

--- js/anime.js
+++ js/anime.js
@@ -20,11 +20,9 @@ fetch('https://api.consumet.org/anime/enime/info?id=' + id)
 <p></p>    `;
         document.getElementById('sidebar').appendChild(sideDataDiv);
         const synopsisDatDiv = document.createElement('div');
-        
         const lowestEpisode = Math.min(...anime.episodes.map(episode => episode.number));
-        
         synopsisDatDiv.innerHTML = `<h1>${anime.title}</h1>
-        <h2>${anime.status} | ${anime.episodes.length} episodes | ${anime.rating} %</h2>
+        <h2>${anime.status} | ${anime.episodes.length} episodes | ${anime.rating}%</h2>
 <p></p>
 <h2>${anime.description}</h2>
 <h2 id="watch"><a href="watch?id=${id}&ep=${anime.episodes.find(episode => episode.number === lowestEpisode).id}&no=${lowestEpisode}"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1.5em" width="2em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.804 8L5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 010 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z" clip-rule="evenodd"></path></svg>Start Watching &nbsp</a></h2>
--- js/home.js
+++ js/home.js
@@ -11,6 +11,69 @@ queryInput.addEventListener("input", function() {
     localStorage.setItem("query", this.value);
 });
 
+const recentEpisodesUrl = 'https://api.consumet.org/anime/gogoanime/top-airing';
+const enimeBaseUrl = 'https://api.consumet.org/anime/enime/';
+
+// Fetch top airing episodes from gogoanime API, search it on enime and display
+fetch(recentEpisodesUrl)
+    .then(response => response.json())
+    .then(data => {
+        const results = data.results;
+        const containerDiv = document.createElement('div'); // Create a container div to hold all the cards
+        containerDiv.style.display = "flex"; // Set the display property of the container div to flex
+        containerDiv.style.flexWrap = "wrap"; // Allow the cards to wrap onto the next line if they can't fit in the same row
+        containerDiv.style.justifyContent = "space-between";
+        containerDiv.style.margin = "20px";
+        results.forEach(result => {
+            const animeTitle = result.title;
+            const enimeSearchUrl = enimeBaseUrl + animeTitle;
+            fetch(enimeSearchUrl)
+                .then(response => response.json())
+                .then(data => {
+                    const enimeResult = data.results[0];
+                    // Display enime result in a card div
+                    const cardDiv = document.createElement('div');
+cardDiv.classList.add('card');
+cardDiv.style.width = "300px";
+cardDiv.style.marginBottom = "20px";
+cardDiv.style.display = "flex";
+cardDiv.style.flexDirection = "column";
+
+const titleElement = document.createElement('h2');
+titleElement.textContent = enimeResult.title;
+titleElement.style.margin = "0";
+
+const linkElement = document.createElement('a');
+linkElement.href = `anime?id=${enimeResult.id}`;
+
+const imageElement = document.createElement('img');
+imageElement.src = enimeResult.image;
+imageElement.style.objectFit = "cover";
+imageElement.style.height = "350px";
+imageElement.style.width = "250px";
+
+const shortTitleElement = document.createElement('h2');
+var title = enimeResult.title;
+var shortTitle = title.substring(0, 50);
+if (title.length > 50)
+    shortTitle += "...";
+shortTitleElement.textContent = shortTitle;
+shortTitleElement.style.margin = "0";
+shortTitleElement.style.fontSize = "28px";
+
+linkElement.appendChild(imageElement);
+linkElement.appendChild(shortTitleElement);
+cardDiv.appendChild(linkElement);
+containerDiv.appendChild(cardDiv);
+
+                })
+        })
+        const header = document.querySelector('h1');
+        header.insertAdjacentElement('afterend', containerDiv); // Add the container div after the h1 element
+    })
+    .catch(error => {
+        console.error('Error fetching anime data: ', error);
+    });
 
 //Code which fetches API and displays autocomplete results
 let autocompleteResults = document.getElementById("autocomplete-results");
